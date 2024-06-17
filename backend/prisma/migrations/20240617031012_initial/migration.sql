045eece7d5e7795c121bd502c969e0e55efdef7f
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-06-17 00:15:38 -0300

    migration

---
+++ backend/prisma/migrations/20240617031012_initial/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Schedule` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `licensePlate` VARCHAR(191) NOT NULL,
+    `washingType` ENUM('SIMPLE', 'FULL') NOT NULL,
+    `startDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `endDate` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
